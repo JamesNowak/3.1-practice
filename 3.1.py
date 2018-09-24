52bafead7f139533aeed66fd790175e8532e46b3
JamesNowak <42552034+JamesNowak@users.noreply.github.com>
2018-09-24 14:06:54 -0500

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,18 @@
+day = int(input("Pick an number: "))
+
+if day == 1:
+    print("You picked Monday!")
+elif day == 2:
+    print("You picked Tuesday!")
+elif day == 3:
+    print("You picked Wednesday!")
+elif day == 4:
+    print("You picked Thursday!")
+elif day == 3:
+    print("You picked Friday!")
+elif day == 7:
+    print("You picked Saturday!")
+elif day == 7:
+    print("You picked Tuesday!")
+else:
+    print("That is not a valid number")
